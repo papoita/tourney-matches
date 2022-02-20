1f7263f8fc4c9811609b94b708906bd73308a384
PaolaP <paolaperez8@gmail.com>
2022-02-20 16:23:42 -0500

    comditional rendering match.js

--- src/components/Match.js
+++ src/components/Match.js
@@ -3,13 +3,13 @@ import React from "react"; //optional
 function Match(props) {
   return (
     <article className="Match">
+
       <h1>
       {props.players[0]} <span>vs</span> {props.players[1]}
       </h1>
       {/* To be shown when there is a winner */}
-      <h2>{props.winner} is the winner by {props.scoreDifference}!</h2>
-      {/* To be shown when there is no winner */}
-      <h2>No winners yet!</h2>
+      {props.winner ? <h2>{props.winner} is the winner by {props.scoreDifference}!</h2> : <h2>No winners yet!</h2>}
+
     </article>
   );
 }
