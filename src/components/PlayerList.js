77aae1906b76765317a61e14660d80d34f153453
PaolaP <paolaperez8@gmail.com>
2022-02-20 15:10:43 -0500

    pass props to match.js and then match.js to

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,8 +1,11 @@
 import playerData from '../data/playerData';
 import matchData from '../data/matchData';
+import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers';
 import Player from "./Player";
 
 function PlayerList(props) {
+  const playerDataArray = preparePlayerData(playerData);
+    const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
