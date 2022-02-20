d6cedfed17f9d0317ed30c9c15029c608738bc7b
PaolaP <paolaperez8@gmail.com>
2022-02-19 22:14:58 -0500

    Add components to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
-import './App.css';
+import "./App.css";
+import MatchList from "./components/MatchList";
+import PlayerList from "./components/PlayerList";
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>
+        Tourney Matches{" "}
+        <span>Where Coding and Tournaments found their Match!</span>
+      </h1>
+      <PlayerList />
+      <MatchList />
     </div>
   );
 }
