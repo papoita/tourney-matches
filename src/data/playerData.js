6942f79f87bb47ee880398a38120515518b2355e
PaolaP <paolaperez8@gmail.com>
2022-02-20 20:25:59 -0500

    add data to display all array

--- src/data/playerData.js
+++ src/data/playerData.js
@@ -4,7 +4,37 @@ const playerData = {
     firstName: "Daria ",
     lastName: "Stark",
   },
-  // more players below...
+  "DrSpiteful": {
+    gamerTag: "DrSpiteful",
+    firstName: "Terrell",
+    lastName: "Hunter",
+  },
+  "Henrikarex": {
+    gamerTag: "Henrikarex",
+    firstName: "Maia",
+    lastName: "Ryder",
+  },
+  "Gamerhorse": {
+    gamerTag: "Gamerhorse",
+    firstName: "Lucien",
+    lastName: "Beltran",
+  },
+  "IHasEyes": {
+    gamerTag: "IHasEyes",
+    firstName:"Matteo",
+    lastName: "Wagstaff",
+  },
+  "Captain Slimy": {
+    gamerTag: "Captain Slimy",
+    firstName: "Persephone",
+    lastName: "Shah",
+  },
+  "H3nr1k4": {
+    gamerTag: "H3nr1k4",
+    firstName: "Elliott",
+    lastName: "Herrera",
+  }
+
 };
 
 export default playerData;
